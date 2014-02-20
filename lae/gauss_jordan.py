d64fa59009866420a6a50b1603bbf76f4fbcf413
kazutoa <ando.kazuto@gmail.com>
2014-02-20 19:27:01 +0900

    convert gauss function to class

--- lae/gauss_jordan.py
+++ lae/gauss_jordan.py
@@ -1,17 +1,25 @@
-def gauss(a):
-  import numpy as np
+import numpy as np
 
-  mat = np.array(a)
-
-  for k in range(len(mat)):
-    mat_kk = mat[k,k]
-    for j in range(len(mat[k])):
-      mat[k,j] = mat[k,j] / mat_kk
-    for i in range(len(mat)):
-      if i == k:
-        continue
-      mat_ik = mat[i,k]
-      for j in range(len(mat[i])):
-        mat[i,j] = mat[i,j] - mat_ik * mat[k,j] 
-
-  return mat[:,3]
+class GaussJordan:
+  def __init__(self, mat, rhs):
+    self.emat = []
+    for row, b in zip(mat, rhs):
+      row.append(b)
+      self.emat.append(row)
+    
+  def solve(self):
+  
+    mat = np.array(self.emat)
+  
+    for k in range(len(mat)):
+      mat_kk = mat[k,k]
+      for j in range(len(mat[k])):
+        mat[k,j] = mat[k,j] / mat_kk
+      for i in range(len(mat)):
+        if i == k:
+          continue
+        mat_ik = mat[i,k]
+        for j in range(len(mat[i])):
+          mat[i,j] = mat[i,j] - mat_ik * mat[k,j] 
+  
+    return mat[:,3]
--- lae/gauss_jordan_test.py
+++ lae/gauss_jordan_test.py
@@ -6,10 +6,12 @@ class GaussJordanTestCase(unittest.TestCase):
     pass
 
   def test_solve(self):
-    a = [[1.0,  1.0, -2.0, 3.0], 
-         [5.0,  2.0,  1.0, 1.0],
-         [1.0, -4.0,  3.0, 8.0]]
-    ret = gauss_jordan.gauss(a)
+    a = [[1.0,  1.0, -2.0], 
+         [5.0,  2.0,  1.0],
+         [1.0, -4.0,  3.0]]
+    rhs = [3.0, 1.0, 8.0]
+    obj = gauss_jordan.GaussJordan(a, rhs)
+    ret = obj.solve()
     self.assertAlmostEqual(ret[0],1.875,3)
     self.assertAlmostEqual(ret[1],-3.125,3)
     self.assertAlmostEqual(ret[2],-2.125,3)

6397bd912c64be14ddc4682a57083ca66d71df0a
kazutoa <ando.kazuto@gmail.com>
2014-02-27 18:16:44 +0900

    convert i and j mutually

--- discretization/elliptic/fdm/laplace_gauss_seidel.py
+++ discretization/elliptic/fdm/laplace_gauss_seidel.py
@@ -11,37 +11,37 @@ def laplace_gauss_seidel():
   u = np.zeros((n+1,m+1))
   uf = np.zeros((n+1,m+1))
 
-  for j in range(n+1):
-    for i in range(m+1):
-      u[j][i] = e
+  for i in range(n+1):
+    for j in range(m+1):
+      u[i][j] = e
 
-  for i in range(m+1):
-    u[0][i] = 0.0
-    u[n][i] = 4 * h * i * (1 - h * i)
+  for j in range(m+1):
+    u[0][j] = 0.0
+    u[n][j] = 4 * h * j * (1 - h * j)
 
-  for j in range(1,n):
-    u[j][0] = 0.0
-    u[j][m] = 0.0
+  for i in range(1,n):
+    u[i][0] = 0.0
+    u[i][m] = 0.0
 
   print("initail u:")
-  for j in range(n+1):
-    for i in range(m+1):
-      print u[j][i],
+  for i in range(n+1):
+    for j in range(m+1):
+      print u[i][j],
     print ""
 
   nitr = 100
   for k in range(nitr):
-    for i in range(1,m):
-      for j in range(1,n):
-        uf[j][i] = u[j][i]
-        u[j][i] = u[j][i] + omega * ((u[j][i+1] + u[j][i-1] + u[j+1][i] + u[j-1][i]) / 4.0 - u[j][i])
+    for j in range(1,m):
+      for i in range(1,n):
+        uf[i][j] = u[i][j]
+        u[i][j] = u[i][j] + omega * ((u[i][j+1] + u[i][j-1] + u[i+1][j] + u[i-1][j]) / 4.0 - u[i][j])
 
     sum1 = 0.0
     sum2 = 0.0
     for j in range(1,n):
       for i in range(1,m):
-        sum1 += abs(u[j][i] - uf[j][i])
-        sum2 += abs(u[j][i])
+        sum1 += abs(u[i][j] - uf[i][j])
+        sum2 += abs(u[i][j])
 
     #print("sum1/sum2={0}".format(sum1/sum2))
     if (sum1 / sum2) <= e:
@@ -50,9 +50,9 @@ def laplace_gauss_seidel():
   print("k = {0}".format(k))
 
   print("result u:")
-  for j in range(n+1):
-    for i in range(m+1):
-      print u[j][i],
+  for i in range(n+1):
+    for j in range(m+1):
+      print u[i][j],
     print ""
 
   return u

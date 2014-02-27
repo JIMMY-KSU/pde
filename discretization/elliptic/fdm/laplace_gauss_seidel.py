9d5dcf561e4bf2865a3f69e021648701af6378bf
kazutoa <ando.kazuto@gmail.com>
2014-02-27 18:25:41 +0900

    refactoring laplace_gauss_seidel

--- discretization/elliptic/fdm/laplace_gauss_seidel.py
+++ discretization/elliptic/fdm/laplace_gauss_seidel.py
@@ -1,61 +1,122 @@
 import numpy as np
+from gauss_jordan import GaussJordan
 
-def laplace_gauss_seidel():
+class Laplace:
+  def __init__(self):
+    pass
 
-  h = 0.1
-  m = 10
-  n = 10
-  e = 0.0001
-  omega = 1.5278
+  def solve(self):
+  
+    #h = 0.1
+    m = 10
+    n = 10
+    e = 0.0001
+    omega = 1.5278
+    ngrids = (m+1)*(n+1)
 
-  u = np.zeros((n+1,m+1))
-  uf = np.zeros((n+1,m+1))
+    h = 0.1
+    k = 0.1
+  
+    u = np.zeros((m+1,n+1))
+    uf = np.zeros((m+1,n+1))
+    a = np.zeros((ngrids,ngrids))
+    c = np.zeros(ngrids)
+    mat = []
+  
+    # set boundary condition
+    for i in range(m+1):
+      for j in range(n+1):
+        #u[i][j] = e
+        u[i][j] = 0.0
+  
+    for j in range(n+1):
+      u[0][j] = 0.0
+      u[m][j] = 4 * h * j * (1 - h * j)
+  
+    for i in range(1,m):
+      u[i][0] = 0.0
+      u[i][n] = 0.0
+  
+    #print("initail u:")
+    #for i in range(n+1):
+    #  for j in range(m+1):
+    #    print u[i][j],
+    #  print ""
 
-  for i in range(n+1):
-    for j in range(m+1):
-      u[i][j] = e
+    # set coefficient matrix
+    for i in range(ngrids):
+      if i >= (n+1) and i < (n+1) * m and (i % (n + 1)) != 0 and ((i - n) % (n + 1)) != 0:
+        for j in range(ngrids):
+          if j == (i - (n + 1)) or j == (i + n + 1):
+            a[i][j] = h * h
+          elif j == (i - 1) or j == (i + 1):
+            a[i][j] = k * k
+          elif j == i:
+            a[i][j] = -2.0 * (h * h + k * k)
 
-  for j in range(m+1):
-    u[0][j] = 0.0
-    u[n][j] = 4 * h * j * (1 - h * j)
+    #print "a:"
+    #for i in range(ngrids):
+    #  for j in range(ngrids):
+    #    print a[i][j],
+    #  print ""
 
-  for i in range(1,n):
-    u[i][0] = 0.0
-    u[i][m] = 0.0
+    #print "u:"
+    #for i in range(m+1):
+    #  for j in range(n+1):
+    #    print u[i,j]
 
-  print("initail u:")
-  for i in range(n+1):
-    for j in range(m+1):
-      print u[i][j],
-    print ""
+    for i in range(ngrids):
+      for j in range(ngrids):
+        c[i] += a[i][j] * u[int(j/(n+1)),j%(n+1)]
 
-  nitr = 100
-  for k in range(nitr):
-    for j in range(1,m):
-      for i in range(1,n):
-        uf[i][j] = u[i][j]
-        u[i][j] = u[i][j] + omega * ((u[i][j+1] + u[i][j-1] + u[i+1][j] + u[i-1][j]) / 4.0 - u[i][j])
+    #print "c:"
+    #for i in range(ngrids):
+    #  print c[i]
 
-    sum1 = 0.0
-    sum2 = 0.0
-    for j in range(1,n):
-      for i in range(1,m):
-        sum1 += abs(u[i][j] - uf[i][j])
-        sum2 += abs(u[i][j])
+    for i in range(ngrids):
+      if i >= (n+1) and i < (n+1) * m and (i % (n + 1)) != 0 and ((i - n) % (n + 1)) != 0:
+        mat.append([])
+        for j in range(ngrids):
+          if j >= (n+1) and j < (n+1) * m and (j % (n + 1)) != 0 and ((j - n) % (n + 1)) != 0:
+            mat[len(mat)-1].append(a[i,j])
+        mat[len(mat)-1].append(-c[i])
 
-    #print("sum1/sum2={0}".format(sum1/sum2))
-    if (sum1 / sum2) <= e:
-      break 
+    #print "mat:"
+    #for row in mat:
+    #  for item in row:
+    #    print item,
+    #  print ""
 
-  print("k = {0}".format(k))
+    mat_ = np.array(mat)
+  
+    lae = GaussJordan()
+    return lae.solve(mat_[:,:len(mat)].tolist(),mat_[:,len(mat)].tolist())
+    
 
-  print("result u:")
-  for i in range(n+1):
-    for j in range(m+1):
-      print u[i][j],
-    print ""
-
-  return u
-
-if __name__ == "__main__":
-  laplace_gauss_seidel()
+    #nitr = 100
+    #for k in range(nitr):
+    #  for j in range(1,n):
+    #    for i in range(1,m):
+    #      uf[i][j] = u[i][j]
+    #      u[i][j] = u[i][j] + omega * ((u[i][j+1] + u[i][j-1] + u[i+1][j] + u[i-1][j]) / 4.0 - u[i][j])
+  
+    #  sum1 = 0.0
+    #  sum2 = 0.0
+    #  for j in range(1,m):
+    #    for i in range(1,n):
+    #      sum1 += abs(u[i][j] - uf[i][j])
+    #      sum2 += abs(u[i][j])
+  
+    #  #print("sum1/sum2={0}".format(sum1/sum2))
+    #  if (sum1 / sum2) <= e:
+    #    break 
+  
+    #print("k = {0}".format(k))
+  
+    #print("result u:")
+    #for i in range(n+1):
+    #  for j in range(m+1):
+    #    print u[i][j],
+    #  print ""
+  
+    #return u
--- discretization/elliptic/fdm/laplace_gauss_seidel_test.py
+++ discretization/elliptic/fdm/laplace_gauss_seidel_test.py
@@ -4,7 +4,7 @@ from gauss_jordan import GaussJordan
 from gauss_seidel import GaussSeidel
 
 class LaplaceTestCase(unittest.TestCase):
-  #def setUp(self):
+  def setUp(self):
   #  nelems = 18
   #  nnodes = 16
   #  width = 1.0
@@ -12,7 +12,7 @@ class LaplaceTestCase(unittest.TestCase):
   #  mesher = Mesher(width, height, nnodes, nelems)
 
   #  lae = GaussJordan()
-  #  self.obj = laplace.Laplace(mesher, lae)
+    self.obj = laplace_gauss_seidel.Laplace()
 
   #  lae2 = GaussSeidel()
   #  self.obj2 = laplace.Laplace(mesher, lae2)
@@ -50,36 +50,29 @@ class LaplaceTestCase(unittest.TestCase):
   #  self.assertAlmostEqual(ret[3],0.513889,6)
 
   def test_solve_with_gauss_seidel(self):
-  #  self.obj2.disc()
-  #  ret = self.obj2.solve()
-    ret = laplace_gauss_seidel.laplace_gauss_seidel()
+    #self.obj.disc()
+    ret = self.obj.solve()
 
-    self.assertAlmostEqual(ret[10][0],0.00,2)
-    self.assertAlmostEqual(ret[10][1],0.36,2)
-    self.assertAlmostEqual(ret[10][2],0.64,2)
-    self.assertAlmostEqual(ret[10][3],0.84,2)
-    self.assertAlmostEqual(ret[9][0],0.00,2)
-    self.assertAlmostEqual(ret[9][1],0.25,2)
-    self.assertAlmostEqual(ret[9][2],0.46,2)
-    self.assertAlmostEqual(ret[9][3],0.61,2)
-    self.assertAlmostEqual(ret[9][4],0.71,2)
-    self.assertAlmostEqual(ret[9][5],0.74,2)
-    self.assertAlmostEqual(ret[9][6],0.71,2)
-    self.assertAlmostEqual(ret[9][7],0.61,2)
-    self.assertAlmostEqual(ret[9][8],0.46,2)
-    self.assertAlmostEqual(ret[9][9],0.25,2)
-    self.assertAlmostEqual(ret[9][10],0.00,2)
-    self.assertAlmostEqual(ret[1][0],0.00,2)
-    self.assertAlmostEqual(ret[1][1],0.01,2)
-    self.assertAlmostEqual(ret[1][2],0.02,2)
-    self.assertAlmostEqual(ret[1][3],0.02,2)
-    self.assertAlmostEqual(ret[1][4],0.03,2)
-    self.assertAlmostEqual(ret[1][5],0.03,2)
-    self.assertAlmostEqual(ret[1][6],0.03,2)
-    self.assertAlmostEqual(ret[1][7],0.02,2)
-    self.assertAlmostEqual(ret[1][8],0.02,2)
-    self.assertAlmostEqual(ret[1][9],0.01,2)
-    self.assertAlmostEqual(ret[1][10],0.00,2)
+    #ret = laplace_gauss_seidel.laplace_gauss_seidel()
+    
+    self.assertAlmostEqual(ret[72],0.25,2)
+    self.assertAlmostEqual(ret[73],0.46,2)
+    self.assertAlmostEqual(ret[74],0.61,2)
+    self.assertAlmostEqual(ret[75],0.71,2)
+    self.assertAlmostEqual(ret[76],0.74,2)
+    self.assertAlmostEqual(ret[77],0.71,2)
+    self.assertAlmostEqual(ret[78],0.61,2)
+    self.assertAlmostEqual(ret[79],0.46,2)
+    self.assertAlmostEqual(ret[80],0.25,2)
+    self.assertAlmostEqual(ret[0],0.01,2)
+    self.assertAlmostEqual(ret[1],0.02,2)
+    self.assertAlmostEqual(ret[2],0.02,2)
+    self.assertAlmostEqual(ret[3],0.03,2)
+    self.assertAlmostEqual(ret[4],0.03,2)
+    self.assertAlmostEqual(ret[5],0.03,2)
+    self.assertAlmostEqual(ret[6],0.02,2)
+    self.assertAlmostEqual(ret[7],0.02,2)
+    self.assertAlmostEqual(ret[8],0.01,2)
 
 if __name__ == "__main__":
   unittest.main()
